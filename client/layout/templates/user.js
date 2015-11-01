01e704dcd1dfaafc1073072babe224b54a7ffa35
Nathaniel Bennett <nathaniel.w.bennett@gmail.com>
2015-11-01 15:27:07 -0800

    added two column layout, styled song divs

--- client/layout/templates/user.js
+++ client/layout/templates/user.js
@@ -1,7 +1,22 @@
-// window.onload = function() {
-//   $(".songClass").click(function(){
-//     console.log("clicked songClass");
-//     var songId = this.id;
-//     player.loadVideoById(songId);
-//   });
-// };
+console.log("song div");
+
+$(document).ready(function() {
+console.log("doc is ready");
+
+
+//SONG DIVS
+  $('.song').hover(
+    function(){
+      $(this).css("background-color", "rgb(194, 244, 220)" );
+    },
+    function(){
+      $(this).css("background-color", "white");
+    }
+  );
+
+  $('.song').click( function(){
+    $('.song').removeClass("focus"); //remove previous focus
+    $(this).addClass("focus"); //set current focus
+  });
+
+});
