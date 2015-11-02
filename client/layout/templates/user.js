d6e4044e4f17ee0f1f33927bf7c413ca316259b4
Do Nguyen <doclimbs@gmail.com>
2015-11-02 10:24:24 -0800

    Removing unneccessary files

--- client/layout/templates/user.js
+++
@@ -1,7 +0,0 @@
-// window.onload = function() {
-//   $(".songClass").click(function(){
-//     console.log("clicked songClass");
-//     var songId = this.id;
-//     player.loadVideoById(songId);
-//   });
-// };
