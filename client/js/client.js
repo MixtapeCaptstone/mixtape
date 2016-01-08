d4697f739aea259ec2aad075ce4c57101d9071d6
Nathaniel Bennett <nathaniel.w.bennett@gmail.com>
2016-01-07 23:54:19 -0800

    Forward button works. Took mongoDB comands out of router.

--- client/js/client.js
+++ client/js/client.js
@@ -3,105 +3,47 @@ ThisPlaylist = new Mongo.Collection(null); //current playlist only on client
 Song = new Mongo.Collection('song');
 Lists = new Mongo.Collection('lists');
 
-
-//Player functions
-
-function playNextSong(){
-  var currentFocus = getSongFocus();
-  var nextFocus = parseInt(currentFocus, 10) + 1;
-  console.log('nextFocus:', nextFocus);
-  setSongFocus(nextFocus);
-  var list = ThisPlaylist.find().fetch();
-  console.log("Running playSongFocus");
-  var songId = list[0].listName[0].playlist[nextFocus].id;
-  player.loadVideoById(songId);
-  player.playVideo();
-}
-
-function playSongFocus(){
-  var currentFocus = getSongFocus();
-  var list = ThisPlaylist.find().fetch();
-  console.log("Running playSongFocus");
-  var songId = list[0].listName[0].playlist[currentFocus].id;
-  console.log("var Song:", songId);
-  player.loadVideoById(songId);
-  player.playVideo();
-}
-
-function setSongFocus(index){
-  console.log('Setting song focus');
-  console.log('index:', index);
-
-  // remove previous focus
-  var currentFocus = getSongFocus();
-  console.log("currentFocus", currentFocus);
-  var removeModifier = { $set: {} };
-  removeModifier.$set["listName.0.playlist." + currentFocus + ".focus"] = false;
-  ThisPlaylist.update({name: 'current'}, removeModifier);
-
-  //set new focus
-  var setModifier = { $set: {} };
-  setModifier.$set["listName.0.playlist." + index + ".focus"] = true;
-  ThisPlaylist.update({name: 'current'}, setModifier);
-  getSongFocus();
-  // ThisPlaylist.update ({name: 'current'}, { '$set': {"listName.0.playlist.1.focus" : true} });
-}
-
-function getSongFocus(){
-  var list = ThisPlaylist.find().fetch();
-  console.log("Running getSongFocus");
-  console.log("getSongFocus list is:", list);
-  var playlist = list[0].listName[0].playlist;
-  var indexValue;
-  console.log("getSongFocus playlist is:", playlist);
-  playlist.forEach(function(element, index, array){
-    var thisFocus = element.focus;
-    console.log("forEach loop:", thisFocus);
-    if(thisFocus === true){
-      console.log("returning index of true", index);
-      indexValue = index;
-    }
-  });
-  return indexValue;
+//Set Initial Playlist
+function setPlayList(params, id){    
+  var findSong = function(){
+      return this;
+  };
+  var reqList = Lists.find({ name: id }).fetch();
+  ThisPlaylist.update(
+    { name: "current" },
+    {
+       name: "current",
+       listName: reqList,
+       findSong: findSong
+    },
+    { upsert: true}
+  );
+  console.log('!!!!!!!!!!!');
+  ThisPlaylist.update ({name: 'current'}, { '$set': {"listName.0.playlist.0.focus" : true} });
 }
 
 // Iron Router
 Router.route('/', function(){
-    this.render('home');
+    this.render('test');
 });
 
-Router.route('/test/:_id', {
+Router.route('/playlist/:_id', {
   action: function () {
      // render all templates and regions for this route
      this.render('test');
-
-
-   },
-   onAfterAction: function () {
      var params = this.params; // { _id: "List Name" }
      var id = params._id; // "List Name"
      Session.set('listName', id);
+     setPlayList(params, id);
      //find playlist object
-     var findSong = function(){
-      return this;
-     };
-     var reqList = Lists.find({ name: id }).fetch();
-     console.log("reqlist", reqList);
-     ThisPlaylist.update(
-        { name: "current" },
-        {
-           name: "current",
-           listName: reqList,
-           findSong: findSong
-        },
-        { upsert: true }
-     );
-     ThisPlaylist.update ({name: 'current'}, { '$set': {"listName.0.playlist.0.focus" : true} });
+
+    //Cue first video
+   },
+   onAfterAction: function () {
+
+    // player.cueVideoById(currentSongId);
    }
 
-
-  // // Session.set('playa', false);
-  // Session.set('YT', true);
 });
 
 Template.test.onRendered(function () {
@@ -115,18 +57,15 @@ Template.test.onRendered(function () {
 Template.user.helpers({
   // This is how to call the database and pass things
   song: function () {
-    // console.log(Song.find({}).fetch());
     var y = Session.get('title');
     var x = y[0].text;
     var name = Meteor.user().username;
     var callback = [];
     var z = Song.find({});
-    console.log(name, x, y);
     z.forEach(function(tune){
       if (tune.username === name)
         callback.push(tune);
     });
-    console.log(callback);
     return callback;
   },
   songClient: function(){
@@ -162,22 +101,8 @@ Template.user.events({
         });
       }
     });
-  },
-  'click .songHover': function (event) {
-    $('#focus').removeAttr('id'); //remove previous focus
-    event.target.id = "focus"; //set focus
-
-    var songId = this.id;
-    player.loadVideoById(songId);
-  },
-  'mouseenter .songName': function(event){
-    thisDiv = event.target; //get "this"
-    $(thisDiv).attr("class", "songHover" );//set hover class
-  },
-  'mouseleave .songHover': function(event){
-    thisDiv = event.target; //get "this"
-    $(thisDiv).attr("class", "songName" ); //remove hover class
   }
+  
 });
 
 Template.searches.helpers({
@@ -349,8 +274,9 @@ Template.listViewer.events({
     // $(thisDiv).attr("class", "songName" ); //remove hover class
   },
   'click .songClass': function (event) {
+
     // $('.focus').attr('class', 'songClass'); //remove previous focus
-    var self = event.target;
+    console.log("clicked song name");
     // $(self).attr( 'class', 'focus');//set focus
 
     var songIndex = $(event.currentTarget).attr("name"); //Get song index number
@@ -371,12 +297,16 @@ Template.playlist.helpers({
 
 Template.playlist.events({
   "click .playlistName": function(event){
-    var newList = Lists.find({name: event.target.id}).fetch();//get playlist
-    var listArray = [];
-    //push individual sing id's into an empty array
-    newList[0].playlist.forEach(function(element, index, array){
-      listArray.push(element.id);
-    });
+    var id = event.target.id;
+    var fullid = "/playlist/" + id;
+    Router.go(fullid);
+
+    // var newList = Lists.find({name: event.target.id}).fetch();//get playlist
+    // var listArray = [];
+    // //push individual sing id's into an empty array
+    // newList[0].playlist.forEach(function(element, index, array){
+    //   listArray.push(element.id);
+    // });
 
     //set state
     // e.preventDefault();
@@ -386,15 +316,15 @@ Template.playlist.events({
     //show playerBox
     $('#playerBox').css('display', 'block');
 
-    //set playlist
-    Session.set("listName", event.target.id);
-    player.loadPlaylist({
-        'playlist': listArray,
-        'listType': 'playlist',
-        'index': 0,
-        'startSeconds': 0,
-        'suggestedQuality': 'small'
-    });
+    // //set playlist
+    // Session.set("listName", event.target.id);
+    // player.loadPlaylist({
+    //     'playlist': listArray,
+    //     'listType': 'playlist',
+    //     'index': 0,
+    //     'startSeconds': 0,
+    //     'suggestedQuality': 'small'
+    // });
 
     SongClient.remove({}); //Remove the client's temporary playlist
     Session.set("title", ""); //Remove Session title
@@ -491,13 +421,13 @@ Template.upvote.events({
   }
 });
 
-Template.home.helpers({
+Template.test.helpers({
   clickCreate: function () {
       return Session.get('clickCreate');
     }
 });
 
-Template.home.events({
+Template.test.events({
   "submit .search": function (event) {
     // Prevent default browser form submit
     event.preventDefault();
@@ -534,7 +464,6 @@ Template.home.events({
 
 Template.player.events({
   "click #escolta": function(){
-    console.log('escolta clicked');
   }
 
 });
@@ -556,7 +485,6 @@ Meteor.startup(function () {
 
   // This event gets triggered when dropped. Puts the dragged item into the new div.
   drop = function (ev) {
-    console.log('dropevent');
     ev.preventDefault();
     var data = ev.dataTransfer.getData("text");
   };
@@ -565,3 +493,360 @@ Meteor.startup(function () {
    passwordSignupFields: 'USERNAME_ONLY'
   });
 });
+
+//YOUTUBE THINGS
+
+
+function playNextSong(){
+  var indexValue = getSongFocus();
+   var list = ThisPlaylist.find().fetch();
+  var playlist = list[0].listName[0].playlist;
+
+  console.log('indexValue:', indexValue, " Playlist.length:", playlist.length);
+
+  if(parseInt(indexValue, 10) >= playlist.length - 1){
+    setSongFocus(0);
+    var songId = list[0].listName[0].playlist[0].id;
+    player.loadVideoById(songId);
+    player.playVideo();
+  } else {
+    var nextFocus = parseInt(indexValue, 10) + 1;
+    console.log('indexValue:', indexValue, ", nextFocus:", nextFocus);
+    var thisId = list[0].listName[0].playlist[nextFocus].id;
+    player.loadVideoById(thisId);
+    player.playVideo();
+    setSongFocus(nextFocus);
+  }
+  console.log("list after change:", playlist);
+}
+
+function playSongFocus(){
+  var currentFocus = getSongFocus();
+  var list = ThisPlaylist.find().fetch();
+  var songId = list[0].listName[0].playlist[currentFocus].id;
+  player.loadVideoById(songId);
+  player.playVideo();
+}
+
+function getCurrentSongId(){
+  var list = ThisPlaylist.find().fetch();
+  var playlist = list[0].listName[0].playlist;
+  var indexValue;
+  playlist.forEach(function(element, index, array){
+    var thisFocus = element.focus;
+    if(thisFocus === true){
+      indexValue = index;
+    }
+  });
+  return list[0].listName[0].playlist[indexValue].id;
+}
+
+function setSongFocus(index){
+  console.log('setSongFocus', index);
+  var list = ThisPlaylist.find().fetch();
+  var playlist = list[0].listName[0].playlist;
+  console.log('current playlist', playlist);
+
+  // remove previous focus
+  var currentFocus = getSongFocus();
+  console.log('current focus:', currentFocus);
+  var removeModifier = { $set: {} };
+  removeModifier.$set["listName.0.playlist." + currentFocus + ".focus"] = false;
+  ThisPlaylist.update({name: 'current'}, removeModifier);
+  // ThisPlaylist.update({name: 'current'}, {"listName.0.playlist.0.focus": false});
+
+
+  //set new focus
+  var setModifier = { $set: {} };
+  setModifier.$set["listName.0.playlist." + index + ".focus"] = true;
+  ThisPlaylist.update({name: 'current'}, setModifier);
+
+  setDivFocus();
+
+  var updatedlist = ThisPlaylist.find().fetch();
+  var updatedplaylist = updatedlist[0].listName[0].playlist;
+  console.log('updated playlist:', updatedplaylist);
+  // ThisPlaylist.update ({name: 'current'}, { '$set': {"listName.0.playlist.1.focus" : true} });
+}
+
+function getSongFocus(){
+  var list = ThisPlaylist.find().fetch();
+  var playlist = list[0].listName[0].playlist;
+  var indexValue;
+  playlist.forEach(function(element, index, array){
+    console.log('getSongFocus index:', index, "element.focus", element.focus);
+    var thisFocus = element.focus;
+    if(thisFocus === true){
+      indexValue = index;
+    }
+  });
+  return indexValue;
+}
+
+function setDivFocus(){
+  var currentFocus = getSongFocus();
+  var x = document.getElementsByClassName("songName");
+
+  //Remove Current Focus
+  for(var i = 0; i < x.length; i++){
+    x[i].id = 'noFocus';    
+  }
+
+  x[currentFocus].id = 'focus';
+}
+
+//YouTube API
+// YouTube API will call onYouTubeIframeAPIReady() when API ready.
+// Make sure it's a global variable.
+onYouTubeIframeAPIReady = function () {
+
+  player = new YT.Player("player", {
+    height: '191',
+    width: '291',
+    videoId: 'M7lc1UVf-VE',
+    playerVars: { 'autoplay': 0, 'controls': 0, 'showinfo': 0 },
+    allowfullscreen: '0',
+      events: {
+        'onReady': onPlayerReady,
+        'onStateChange': onPlayerStateChange
+      }
+  });
+
+};
+
+YT.load();
+
+function onPlayerReady(event) {
+
+    var artist = player.getVideoData();
+    $('#artistName').text(artist.author + " " + artist.title);
+
+
+    $('.escolta').unbind("click");
+
+    $('.escolta').on('click', function() {
+
+        if(event.data === 1) {
+            player.pauseVideo();
+        }
+        else {
+          setSongFocus();
+          player.playVideo();
+          setDivFocus()
+        }
+        return false;
+    });
+
+    $('#forward').on('click', function(){
+      playNextSong();
+      // var songIndex = getSongFocus() + 1;
+      // console.log("songIdex for #forward:", songIndex);
+      // setSongFocus(songIndex);
+      // playSongFocus();  
+    });
+
+    var minutes = Math.floor((player.getDuration()) / 60);
+    var seconds = player.getDuration() % 60;
+    $('.duration').text(minutes + ':' + seconds);
+
+//SCRUBBER
+
+    var timeDrag = false;   /* Drag status */
+    $('.progressParent').mousedown(function(e) {
+        timeDrag = true;
+        updatebar(e.pageX);
+    });
+
+    $('.progressParent').mouseup(function(e) {
+        if(timeDrag) {
+            timeDrag = false;
+            updatebar(e.pageX);
+        }
+    });
+    $('.progressParent').mousemove(function(e) {
+        if(timeDrag) {
+            updatebar(e.pageX);
+        }
+    });
+
+    //update Progress Bar control
+    var updatebar = function(x) {
+        var progress = $('.progressParent');
+        var maxduration = player.getDuration(); //Video duraiton
+        var position = x - progress.offset().left; //Click pos
+        var percentage = 100 * position / progress.width();
+
+        //Check within range
+        if(percentage > 100) {
+            percentage = 100;
+        }
+        if(percentage < 0) {
+            percentage = 0;
+        }
+
+        //Update progress bar and video currenttime
+        $('.progress').css('width', percentage + '%');
+        player.seekTo(maxduration * percentage / 100);
+    };
+
+    //Volume DIVS
+    $('.volumeDiv').hover(
+      function(){
+        $(this).animate({'margin-top': '2px'});
+      },
+      function(){
+        $(this).animate({"margin-top": "5px"});
+      }
+    );
+
+    $('.volumeDiv').click(
+      function(){
+        var counter = parseInt($(this).attr('id'));
+
+        player.setVolume(counter);
+
+        $('.volumeDiv').css('background-color', 'rgb(143, 143, 143)');
+        for(var i =0; i <= counter; i++){
+          var divID = "#" + i;
+          $(divID).css('background-color', "rgb(255, 255, 255)");
+        }
+      }
+    );
+
+    // var currentSongId = getCurrentSongId();
+    // player.cueVideoById(currentSongId);
+}
+
+var scrubber;
+
+function setTimer(event){
+  var num = event.data;
+  var currentTime = player.getCurrentTime();
+  var time = currentTime;
+  scrubber = setInterval(clock, 150);
+  if (event.data != 1){
+    clearInterval(scrubber);
+  }
+}
+
+function clock(){
+ var time = player.getCurrentTime();
+ var duration = player.getDuration();
+ $('.current').text(time);
+ var percentage = 100 * time / duration; //in %
+ $('.progress').css('width', percentage + '%');
+}
+
+
+
+
+var done = false;
+function onPlayerStateChange(event) {
+
+
+  //SET FOCUS
+  $('.focus').attr('class', 'songClass'); //remove previous focus
+  playIndex = event.target.A.playlistIndex;
+  var thisDiv = $('div[name="' + playIndex + '"]');
+  var child = $(thisDiv).children('div');
+  $(child).attr("class","focus");
+//---------------------------------
+  $('.escolta').unbind("click");
+
+  //Change the play button text
+  if(event.data === 1) {
+      $('.escolta').html('<svg class="icon-pause2"><use xlink:href="#icon-pause2"></use></svg>');
+  }
+  else {
+    $('.escolta').html('<svg class="icon-play3"><use xlink:href="#icon-play3"></use>');
+  }
+
+  $('.escolta').on('click', function() {
+
+    clearInterval(scrubber);
+      if(event.data === 1) {
+          player.pauseVideo();
+      }
+      else {
+        player.playVideo();
+        getSongFocus();
+        setDivFocus()
+      }
+      return false;
+  });
+
+  setTimer(event);
+//
+}
+
+
+$(document).ready(function(){
+
+
+//SCRUBBER
+
+var timeDrag = false;   /* Drag status */
+
+$('.progressParent').mousedown(function(e) {
+    timeDrag = true;
+    updatebar(e.pageX);
+});
+
+$('.progressParent').mouseup(function(e) {
+    if(timeDrag) {
+        timeDrag = false;
+        updatebar(e.pageX);
+    }
+});
+$('.progressParent').mousemove(function(e) {
+    if(timeDrag) {
+        updatebar(e.pageX);
+    }
+});
+
+//update Progress Bar control
+var updatebar = function(x) {
+    var progress = $('.progressParent');
+    var maxduration = player.getDuration(); //Video duraiton
+    var position = x - progress.offset().left; //Click pos
+    var percentage = 100 * position / progress.width();
+
+    //Check within range
+    if(percentage > 100) {
+        percentage = 100;
+    }
+    if(percentage < 0) {
+        percentage = 0;
+    }
+
+    //Update progress bar and video currenttime
+    $('.progress').css('width', percentage + '%');
+    player.seekTo(maxduration * percentage / 100);
+};
+
+//Volume DIVS
+$('.volumeDiv').hover(
+  function(){
+    $(this).animate({'margin-top': '2px'});
+  },
+  function(){
+    $(this).animate({"margin-top": "5px"});
+  }
+);
+
+$('.volumeDiv').click(
+  function(){
+    var counter = parseInt($(this).attr('id'));
+
+    player.setVolume(counter);
+
+    $('.volumeDiv').css('background-color', 'rgb(143, 143, 143)');
+    for(var i =0; i <= counter; i++){
+      var divID = "#" + i;
+      $(divID).css('background-color', "rgb(255, 255, 255)");
+    }
+  }
+);
+
+});
+
